d9faa42263162a11247a4bdd245aec507931e820
N S Balaji <balajinapa123@gmail.com>
2026-07-04 06:10:37 -0700

    Time: 5 ms (53.22%), Space: 45.4 MB (20.99%) - LeetHub

---
+++ 0071-simplify-path/0071-simplify-path.java
@@ -0,0 +1,28 @@
+class Solution {
+    public String simplifyPath(String path) {
+        String[] components = path.split("/");
+        Stack<String> st = new Stack<>();
+
+        for(String comp : components){
+            if(comp.equals("") || comp.equals(".")){
+                continue;
+            }
+            if(comp.equals("..")){
+                if(!st.isEmpty()){
+                    st.pop();
+                }
+            }else{
+                st.push(comp);
+            }
+        }  
+        StringBuilder sb = new StringBuilder();
+        while(!st.isEmpty()){
+            sb.insert(0, "/" + st.pop());
+        }
+        if(sb.length() == 0){
+            return "/";
+        }else{
+            return sb.toString();
+        }
+    }
+}
