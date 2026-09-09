418a072a09211d743e773835d2257b153cdcb9a1
N S Balaji <balajinapa123@gmail.com>
2026-09-09 23:33:00 +0530

    Time: 25 ms (76.82%), Space: 44.3 MB (40.5%) - LeetHub

---
+++ 0133-clone-graph/0133-clone-graph.java
@@ -0,0 +1,45 @@
+/*
+Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
+
+class Solution {
+    public Node cloneGraph(Node node) {
+        if(node == null)return null;
+        Map<Node,Node> map = new HashMap<>();
+        Queue<Node> q = new LinkedList<>();
+        q.add(node);
+        map.put(node, new Node(node.val));
+        while(!q.isEmpty()){
+            Node first = q.remove();
+            Node firstClone = map.get(first);
+            for(Node curr : first.neighbors){
+                Node currClone;
+                if(map.containsKey(curr)){
+                    currClone = map.get(curr);
+                }else{
+                    q.add(curr);
+                    currClone = new Node(curr.val);
+                    map.put(curr, currClone);
+                }
+                firstClone.neighbors.add(currClone);
+            }
+        }
+        return map.get(node);
+    }
+}
